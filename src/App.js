d7dea046198af8c7acaa21ef9227792cdda95330
zeus <zeus@pop-os.localdomain>
2020-02-24 14:09:44 +0530

    Hooks vala

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import React, { Component } from 'react';
-import './App.css';
-import {connect} from 'react-redux';
+// import React, { Component } from 'react';
+// import './App.css';
+// import {connect} from 'react-redux';
+// import {userAction} from './redux/actions/userAction';
+
+// class App extends Component {
+  
+//   fetchUsers = () => {
+//     this.props.userFetch()
+//   }
+
+//   consoleUser = () => {
+//     let res = this.props.users
+//     res.then((res) => console.log(res))
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.fetchUsers}>Fetch user</button>
+//         <button onClick={this.consoleUser}>Console User</button>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = () => state => ({
+//   users: state.userFetch.users
+// })
+
+// const mapDispatchToProps = dispatch => ({
+//   userFetch: () => dispatch(userAction())
+// })
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {userAction} from './redux/actions/userAction';
 
-class App extends Component {
-  
-  fetchUsers = () => {
-    this.props.userFetch()
-  }
+function App() {
+  const users = useSelector(state => state.userFetch.users)
+  const dispatch = useDispatch();
 
-  consoleUser = () => {
-    let res = this.props.users
-    res.then((res) => console.log(res))
-  }
-  
-  render() {
-    return (
-      <div>
-        <button onClick={this.fetchUsers}>Fetch user</button>
-        <button onClick={this.consoleUser}>Console User</button>
-      </div>
-    );
-  }
+  // users.then((res) => console.log(res))
+
+  return(
+    <div>
+      <button onClick={() => dispatch(userAction())}>Fetch User</button>
+      <button onClick={() => users.then(res => console.log(res))}>Resolve User</button>
+    </div>
+  )
 }
 
-const mapStateToProps = () => state => ({
-  users: state.userFetch.users
-})
-
-const mapDispatchToProps = dispatch => ({
-  userFetch: () => dispatch(userAction())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
